63b082fa97774c31e3bce043fda8d95b469a2aed
Nathan Danque <natecgxpstudio@hotmail.com>
2013-07-02 11:41:54 -0700

    Initial screenscrolling

--- src/game/TechDemoLevel.java
+++ src/game/TechDemoLevel.java
@@ -8,6 +8,7 @@ package game;
 
 //imports
 
+import com.Enjyn.Block;
 import com.Enjyn.PlayerClass;
 import com.Enjyn.BlockMap;
 import com.Enjyn.Camera;
@@ -24,7 +25,8 @@ public class TechDemoLevel extends BasicGameState {
     private BlockMap bMap;
     private PlayerClass player;
     private Vector2f startPos;
-    private Camera cam;
+    private float mapX, mapY;
+
     public TechDemoLevel(int id)
     {
     
@@ -36,20 +38,35 @@ public class TechDemoLevel extends BasicGameState {
         startPos = new Vector2f(100, 650);
         bMap = new BlockMap("./res/demoMap.tmx");
         player = new PlayerClass(startPos, 64, 64, .25f, 0);
-        cam = new Camera(gc);
+        mapX = 0;
+        mapY = 0;
+
     }
     
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException
     {
-        player.setControl(gc, delta, bMap);
-        cam.update(gc, delta, bMap);
+        player.setControl(gc, delta, bMap);     
+        for(int i = 0; i < bMap.entities.size(); i++)
+        {
+            Block tile = (Block) bMap.entities.get(i);
+            if(player.getKeyPressed() && gc.getInput().isKeyDown(Input.KEY_D))
+            {
+                mapX -= (float)0.001 * delta;
+                tile.poly.setX(mapX);
+            }
+            
+            if(player.getKeyPressed() && gc.getInput().isKeyDown(Input.KEY_A));
+            {
+                mapX += (float)0.001 * delta;
+                tile.poly.setX(mapX);
+            }
+        }
     }
     
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
     {
-        cam.Render(g);
-        bMap.tmap.render(0, 0);
+        bMap.tmap.render((int)mapX, (int)mapY);
         g.draw(player.getPolygon());
     }
     

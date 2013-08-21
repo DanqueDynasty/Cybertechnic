bb668497f266949d6eda8efe569dd24de7fa3e5b
Nathan Danque <natecgxpstudio@hotmail.com>
2013-08-20 21:57:27 -0700

    lvl2Start

--- src/game/Level_02.java
+++ src/game/Level_02.java
@@ -13,14 +13,15 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import com.Enjyn.*;
+import org.newdawn.slick.geom.Vector2f;
 
 public class Level_02 extends BasicGameState {
     
     private BlockMap bmap;
     private boolean isPause;
     private boolean isGameOver;
-    private PlayerClass player;
-    private int score;
+    private static PlayerClass player;
+    private static int score;
     
     public Level_02(int id)
     {
@@ -31,21 +32,58 @@ public class Level_02 extends BasicGameState {
     {
         this.score = score;
         this.player = p;
+        player.setVector(new Vector2f(0, 1000));
     }
     
     public void init(GameContainer gc, StateBasedGame sbg)throws SlickException
     {
-        
+        bmap = new BlockMap("./res/map/Level_02.tmx");
+        isGameOver = false;
+        isPause = false;
     }
     
     public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException
     {
-        System.out.println("In Level2");
+        if(!isGameOver)
+        {
+            if(!isPause)
+            {
+                player.setControl(gc, delta, bmap);
+                player.update(delta);
+                
+            }
+        }
+        else
+        {
+            //sbg.enterState(0);
+        }
     }
     
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException
     {
-    
+        if(!isGameOver)
+        {
+            if(!isPause)
+            {
+                float xOffset = 0;
+                float yOffset = 435;
+                
+                float mapX = bmap.tmap.getWidth() - player.getVector().getX() + xOffset;
+                float mapY = bmap.tmap.getHeight() - player.getVector().getY() + yOffset;
+                
+                g.translate(mapX, mapY);
+                player.render(g);
+                bmap.tmap.render(0, 0);
+                //temp
+                g.draw(player.getPolygon());
+                g.drawImage(player.getMasterImage(), 
+                            player.getVector().getX(),
+                            player.getVector().getY());
+                g.resetTransform();
+                g.drawString("Score: " + score, 0, 0);
+                g.drawString("Health: " + player.getHealth(), 0, 32);  
+            }
+        }
     }
     
     public int getID()
